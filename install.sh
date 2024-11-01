f31ee790d2dd602e8c7f210fa754aa8604cbc2af
Windaa014 <oyoy1413@gmail.com>
2024-11-01 22:35:08 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,17 +13,17 @@ display_welcome() {
   echo -e "${BLUE}[+] =============================================== [+]${NC}"
   echo -e "${BLUE}[+]                                                 [+]${NC}"
   echo -e "${BLUE}[+]                AUTO INSTALLER THEMA             [+]${NC}"
-  echo -e "${BLUE}[+]                  © RafatharCode                [+]${NC}"
+  echo -e "${BLUE}[+]                  © WINDAHOSTING                 [+]${NC}"
   echo -e "${BLUE}[+]                                                 [+]${NC}"
   echo -e "${RED}[+] =============================================== [+]${NC}"
   echo -e ""
-  echo -e "script ini di buat untuk mempermudah penginstalasian thema pterodactyle,"
-  echo -e "dilarang keras untuk dikasih gratis."
+  echo -e "script ini free untuk kalian pakai sesuka hati kalian,"
+  echo -e "semoga membantu pekerjaan anda ©windhost"
   echo -e ""
   echo -e "𝗧𝗘𝗟𝗘𝗚𝗥𝗔𝗠 :"
-  echo -e "@RafatharCode"
+  echo -e "@OsideGirl"
   echo -e "𝗖𝗥𝗘𝗗𝗜𝗧𝗦 :"
-  echo -e "@RafatharCode"
+  echo -e "@windhost"
   sleep 4
   clear
 }
@@ -56,20 +56,20 @@ install_jq() {
 check_token() {
   echo -e "                                                       "
   echo -e "${BLUE}[+] =============================================== [+]${NC}"
-  echo -e "${BLUE}[+]               LICENSY RAFATHAR CODE            [+]${NC}"
+  echo -e "${BLUE}[+]               LICENSY WINDAHOSTING              [+]${NC}"
   echo -e "${BLUE}[+] =============================================== [+]${NC}"
   echo -e "                                                       "
   echo -e "${YELLOW}MASUKAN AKSES TOKEN :${NC}"
   read -r USER_TOKEN
 
-  if [ "$USER_TOKEN" = "happyalfixamalia" ]; then
+  if [ "$USER_TOKEN" = "windahosting" ]; then
     echo -e "${GREEN}AKSES BERHASIL${NC}}"
   else
-    echo -e "${GREEN}Buy dulu Gih Ke RafatharCode X Amalia${NC}"
-    echo -e "${YELLOW}TELEGRAM : @RafatharCode${NC}"
-    echo -e "${YELLOW}WHATSAPP : 6285864743922${NC}"
-    echo -e "${YELLOW}HARGA TOKEN : 25K FREE UPDATE JIKA ADA TOKEN BARU${NC}"
-    echo -e "${YELLOW}©RafatharCode${NC}"
+    echo -e "${GREEN}Selamat datang di auto install by windhost${NC}"
+    echo -e "${YELLOW}selamat memakai script dari windhost{NC}"
+    echo -e "${YELLOW}ini script free ya buat kalian${NC}"
+    echo -e "${YELLOW}jadi jangan di jual belikan${NC}"
+    echo -e "${YELLOW}©WindHost${NC}"
     exit 1
   fi
   clear
@@ -452,11 +452,11 @@ while true; do
   echo -e "${BLUE}  ,ggggg        gggggggg.                                 ${NC}"
   echo -e "${BLUE} ,ggg'               'ggg.                                ${NC}"
   echo -e "${BLUE}',gg       ,ggg.      'ggg:                               ${NC}"
-  echo -e "${BLUE}'ggg      ,gg'''  .    ggg       Auto Installer RafatharCode Private   ${NC}"
-  echo -e "${BLUE}gggg      gg     ,     ggg      ------------------------  ${NC}"
-  echo -e "${BLUE}ggg:     gg.     -   ,ggg       • Telegram : t.me/RafatharCode     ${NC}"
-  echo -e "${BLUE} ggg:     ggg._    _,ggg        • Creadit  : RAFATHARCODE ${NC}"
-  echo -e "${BLUE} ggg.    '.'''ggggggp           • Support by RafatharCode X Amalia  ${NC}"
+  echo -e "${BLUE}'ggg      ,gg'''  .    ggg       Auto Installer WindaHosting   ${NC}"
+  echo -e "${BLUE}gggg      gg     ,     ggg      ------------------------       ${NC}"
+  echo -e "${BLUE}ggg:     gg.     -   ,ggg       • Telegram : @OsideGirl        ${NC}"
+  echo -e "${BLUE} ggg:     ggg._    _,ggg        • Creadit  : WINDAHOSTING      ${NC}"
+  echo -e "${BLUE} ggg.    '.'''ggggggp           • Support by Windhost     ${NC}"
   echo -e "${BLUE}  'ggg    '-.__                                           ${NC}"
   echo -e "${BLUE}    ggg                                                   ${NC}"
   echo -e "${BLUE}      ggg                                                 ${NC}"
